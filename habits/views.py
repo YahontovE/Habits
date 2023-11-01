b36988137ea90864479ae0bcad1756ff7b5474e5
Evgen <yahontov-sport@inbox.ru>
2023-11-01 13:31:21 +0300

    add cod in views file

--- habits/views.py
+++ habits/views.py
@@ -1,21 +1,56 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
+from habits.models import Habit
+from habits.serializers import HabitSerializer
 
 
 class HabitCreateAPIView(generics.CreateAPIView):
-    pass
+    serializer_class = HabitSerializer
+    permission_classes = [AllowAny]#~IsModer
+
+    # def perform_create(self, serializer):
+    #     new_habit = serializer.save()
+    #     new_habit.user = self.request.user
+    #     new_habit.save()
 
 
 class HabitListAPIView(generics.ListAPIView):
-    pass
+    """Эндпоинт просмотра всех привычек"""
+    serializer_class = HabitSerializer
+    #pagination_class = HabitPaginator
+    #permission_classes = [IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return Habit.objects.filter(owner=self.request.user).order_by('id')
+
+class PublicHabitListAPIView(generics.ListAPIView):
+    """Эндпоинт просмотра всех публичных привычек"""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.filter(is_public=True).order_by('-id')
+    #pagination_class = HabitPaginator
+    #permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class HabitUpdateAPIView(generics.UpdateAPIView):
-    pass
+    """Эндпоинт редактирования привычки"""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    #permission_classes = [IsOwner]
 
+    def perform_update(self, serializer):
+        updated_habit = serializer.save()
+        updated_habit.owner = self.request.user
+        updated_habit.save()
 
 class HabitRetrieveAPIView(generics.RetrieveAPIView):
-    pass
+    """Эндпоинт просмотра определенной привычки"""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    #permission_classes = [IsOwner]
 
 
 class HabitDestroyAPIView(generics.DestroyAPIView):
-    pass
+    """Эндпоинт удаления привычки"""
+    queryset = Habit.objects.all()
+    #permission_classes = [IsOwner]
